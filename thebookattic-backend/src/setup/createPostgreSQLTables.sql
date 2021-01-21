795f1bb5b6baebb0fd743124e8bfa422dce5a1b7
Robert Thompson <rthompson48@hawkmail.hccfl.edu>
2021-01-21 18:57:11 -0500

    changed bool to boolean in createTable

--- thebookattic-backend/src/setup/createPostgreSQLTables.sql
+++ thebookattic-backend/src/setup/createPostgreSQLTables.sql
@@ -7,7 +7,7 @@ create table books
 	blurb varchar(500) not null,
 	page_count int not null,
 	link varchar(50),
-	approved bool default false,
+	approved boolean default false,
 	genreid int not null,
 	constraint fk_authorid foreign key (authorid) references authors (id),
 	constraint fk_genreid foreign key (genreid) references genres (id)
