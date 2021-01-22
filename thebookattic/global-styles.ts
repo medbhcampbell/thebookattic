b303643c318c83d7ba18068af771b880dfd88087
Madhavi Thurlapati <madhavi.thurlapati@revature.net>
2021-01-21 17:42:15 -0700

    book attic backend with login

---
+++ thebookattic/global-styles.ts
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        borderColor: '#880022',
+        borderStyle: 'solid',
+        borderWidth: 3
+    },
+    input: {
+        backgroundColor: '#fff',
+        borderRadius: 20
+    },
+    login: {
+        backgroundColor: '#aaa'
+    },
+   
+});
+
+export default styles;
