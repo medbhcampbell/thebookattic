47844eee77a71effb10aaf84da8e302462396431
Robert Thompson <rthompson48@hawkmail.hccfl.edu>
2021-01-29 11:19:42 -0500

    removed unneeded testing code

--- thebookattic/author/authordetail.component.tsx
+++ thebookattic/author/authordetail.component.tsx
@@ -19,59 +19,6 @@ export default function AuthorDetailComponent() {
     const author = useSelector(selectAuthor);
     const selectBooks = (state: BookState) => state.books;
     const books = useSelector(selectBooks);
-
-    // useEffect(() => {
-    //     authorService.getAuthorById(1).then((author) => {
-    //         dispatch(getAuthor(author));
-    //     })
-    // }, [dispatch]);
-    console.log('AuthorDetailComponent - books: ' + JSON.stringify(books));
-    console.log('AuthorDetailComponent - author: ' + JSON.stringify(author));
-    let newAuthor = new Author();
-    newAuthor.authorid = 9;
-    newAuthor.userid = 111116;
-    newAuthor.firstname = 'Joe';
-    newAuthor.lastname = 'Author';
-    newAuthor.avgrating = 3.7;
-    newAuthor.bio = 'bio';
-    newAuthor.picture = 'url';
-
-    let updatedAuthor = new Author();
-    updatedAuthor.authorid = 7;
-    updatedAuthor.userid = 111111;
-    updatedAuthor.firstname = 'Jake';
-    updatedAuthor.lastname = 'Austin';
-    updatedAuthor.avgrating = 5.0;
-    updatedAuthor.bio = 'Very English';
-    updatedAuthor.picture = 'url';
-
-    console.log(newAuthor);
-    console.log(updatedAuthor);
-    
-    function onAddSelect() {
-        authorService.addAuthor(newAuthor);
-    }
-
-    function onUpdateSelect() {
-        authorService.updateAuthor(updatedAuthor);
-    }
-
-    function onRemoveSelect() {
-        authorService.removeAuthor('7');
-    }
-
-    function onBookSelect(index: number) {
-        const book = books[index];
-        navigation.navigate('BookDetail', book);
-    }
-
-    function onGetAuthorSelect() {
-        //let a: any = { ...author };
-        authorService.getAuthorById(1).then((author) => {
-            console.log(author);
-        });
-        //console.log(a.firstname + ' ' + a.lastname);
-    }
     
     return (
         <ScrollView>
