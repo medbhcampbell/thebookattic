d1db4660e34fa83630ab225cf283a93e8815269a
Emma (Yuqing) Wang <wangyuqingxiao@gmail.com>
2021-01-29 00:07:42 -0500

    updated filter books

--- thebookattic/book/booklist.component.tsx
+++ thebookattic/book/booklist.component.tsx
@@ -23,20 +23,24 @@ export default function BookListComponent(props: BookListProps) {
     const navigation = useNavigation();
     const dispatch = useDispatch();
     const genres = useSelector((state: GenreState) => state.genres);
-    const [filter, setFilter] = useState(0);
-    const approved = props.books.filter(item=>{return item.approved});
-    const [filteredList, setList] = useState(approved);
+    const [filter, setFilter] = useState('');
+    //have to render approved when no filter is applied
+    //because useState is async and it's not using initial state upon first few renders
+    let approved = props.books.filter(item=>{return item.approved});
+    const [list, setList] = useState<Book[]>([]);
     function onBookSelect(index: number) {
         const book = props.books[index];
         navigation.navigate('BookDetail', book);
     }
-
+    //set new list whenever user changes a genre
+    //if no filter applied it's going to be a list of approved books
     function handleFilter(itemValue: any){
         setFilter(itemValue);
-        const newList = approved.filter((item)=>{
-            return itemValue !== 0 && item.genreid == itemValue;
-        });
-        setList(newList);
+        if(itemValue){
+            setList(approved.filter(item=>item.genreid == itemValue));        
+        }else{
+            setList(approved);
+        }
     }
 
     useEffect(() => {
@@ -66,8 +70,10 @@ export default function BookListComponent(props: BookListProps) {
         <View style={{alignItems: 'center'}}>
             <Picker
             selectedValue={filter}
-            style={{height: 50, width: 100}}
             onValueChange={handleFilter}>
+                <Picker.Item
+                    label='No Filter'
+                    value='' />
                 {genres.length > 0 && genres.map((genre) => {
                 return <Picker.Item
                     key={genre.id}
@@ -76,10 +82,10 @@ export default function BookListComponent(props: BookListProps) {
                 })}
             </Picker>
             {props.retrievedBooks?
-                (filteredList.length > 0) ? 
-                    <FlatList data={filteredList} renderItem={BookPreview} keyExtractor={keyExtractor}/>
-                    : <Text style={style.h1}> No books found!</Text>
-                : <ActivityIndicator/>}
+                ((filter == '' && approved.length > 0) || list.length > 0) ? 
+                    <FlatList data={filter == '' ? approved : list} renderItem={BookPreview} keyExtractor={keyExtractor}/>
+                : <Text style={style.h1}> No books found!</Text>
+            : <ActivityIndicator/>}
         </View>
     )
 }
