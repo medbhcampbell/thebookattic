5a045ab8b292b2f8644faf325158de7826b83a94
Robert Thompson <rthompson48@hawkmail.hccfl.edu>
2021-01-29 20:35:26 -0500

    added logout functionality, books now display author's name, minor syntax fixes

--- thebookattic/book/bookdetail.component.tsx
+++ thebookattic/book/bookdetail.component.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { View, Text, Image } from 'react-native';
 import { RouteProp, useNavigation } from '@react-navigation/native';
-
 import { StackParams } from '../router/router.component';
+
+import { getAuthor } from '../store/actions';
 import { Book } from './book';
 import { useDispatch, useSelector } from 'react-redux';
-import { UserState } from '../store/store';
+import { UserState, AuthorState } from '../store/store';
 import bookService from './book.service';
 import authorService from '../author/author.service';
 import DeleteBookComponent from './deletebook.component';
@@ -15,12 +16,21 @@ interface BookDetailProps {
 }
 
 export default function BookDetailComponent(props: BookDetailProps) {
-
+    const dispatch = useDispatch();
     const book: Book = props.route.params;
+    const selectAuthor = (state: AuthorState) => state.author;
+    const author = useSelector(selectAuthor);
+
 
     //check if this user is the book's author
     const user = useSelector((state: UserState) => state.user);
     const [userIsAuthor, setUserIsAuthor] = useState(false);
+
+    useEffect(() => {
+        authorService.getAuthorById(book.authorid).then((author) => {
+            dispatch(getAuthor(author));
+        });
+    }, []);
 
     useEffect(() => {
         //useEffect callback cannot be async, this lets us use await 
@@ -45,7 +55,7 @@ export default function BookDetailComponent(props: BookDetailProps) {
         <View>
             <Image source={{ uri: book.cover }}></Image>
             <Text>{book.title}</Text>
-            <Text>Author: TODO getAuthorByID</Text>
+            <Text>Author: {author.firstname + ' ' + author.lastname}</Text>
             {book.link &&
                 <Text>Access it here: {book.link}</Text>}
             <Text>{book.blurb}</Text>
