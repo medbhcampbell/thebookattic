5ad4f002bff28171edae2944ae2e0f784dea6769
Medbh Campbell <medbh.campbell53@gmail.com>
2021-02-02 17:38:13 -0600

    more and more authors and books

--- thebookattic-backend/src/setup/createPostgreSQLTables.sql
+++ thebookattic-backend/src/setup/createPostgreSQLTables.sql
@@ -76,48 +76,48 @@ insert into genres (name) values ('sci-fi');
 insert into genres (name) values ('young adult');
 insert into genres (name) values ('horror');
 
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('jausten', 'Jane', 'Austen', '4.5', 'English romance author for the gentry', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('wshakespeare', 'William', 'Shakespeare', '2.7', 'Some unpopular English scriptwriter', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('ltolstoy', 'Leo', 'Tolstoy', '4.8', 'Russian author who writes sad, preachy stories', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('sking', 'Stephen', 'King', '4.5', 'Writes scary stuff', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('rdahl', 'Roald', 'Dahl', '4.6', 'Likes traumatizing children', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('ifleming', 'Ian', 'Fleming', '3.8', 'Likes his spy dramas shaken, not stirred', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('jverne', 'Jules', 'Verne', '4.6', 'French travel writer', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('csagan', 'Carl', 'Sagan', '4.6', 'The man who inspired billions and billions of physicists (probably)', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('gmartin', 'George RR', 'Martin', '4.7', 'Likes traumatizing adults', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('jtolkien', 'JRR', 'Tolkien', '4.9', 'Obsessed with jewellery and elves', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('achristie', 'Agatha', 'Christie', '5.0', 'Undisputed queen of the whoddunnit', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('mshelley', 'Mary', 'Shelley', '5.0', 'Started a genre in a thunderstorm', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('dadams', 'Douglas', 'Adams', '4.6', 'A hoopy frood who knows where his towel is', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('epoe', 'Edgar Allen', 'Poe', '4.9', 'The gothiest goth to ever goth', 'url');
-insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('egaskell', 'Elizabeth', 'Gaskell', '4.6', 'Austens goth twin', 'url');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('jausten', 'Jane', 'Austen', '4.5', 'English romance author for the gentry', 'https://www.history.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTU3OTIzNTc2MTE1NzAxMzk0/why-jane-austen-never-marrieds-featured-photo.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('wshakespeare', 'William', 'Shakespeare', '2.7', 'Some unpopular English scriptwriter', 'https://www.bl.uk/britishlibrary/~/media/bl/global/dl%20shakespeare/authors/shakespeare-people-page.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('ltolstoy', 'Leo', 'Tolstoy', '4.8', 'Russian author who writes sad, preachy stories', 'https://cdn.britannica.com/94/4694-050-CABE0BB0/Leo-Tolstoy.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('sking', 'Stephen', 'King', '4.5', 'Writes scary stuff', 'https://images.gr-assets.com/authors/1362814142p8/3389.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('rdahl', 'Roald', 'Dahl', '4.6', 'Likes traumatizing children', 'https://www.biography.com/.image/ar_1:1%2Cc_fill%2Ccs_srgb%2Cg_face%2Cq_auto:good%2Cw_300/MTQxNDAyNjQ0MzAzMzI1MDc0/roald_dahl_getty_images_tony_evansgetty_images_108874289_croppedjpg.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('ifleming', 'Ian', 'Fleming', '3.8', 'Likes his spy dramas shaken, not stirred', 'https://cdn.britannica.com/60/176960-050-6676378E/Ian-Fleming.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('jverne', 'Jules', 'Verne', '4.6', 'French travel writer', 'https://www.biography.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cq_auto:good%2Cw_1200/MTE4MDAzNDEwNzEzNjc1Mjc4/jules-verne-9517579-1-402.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('csagan', 'Carl', 'Sagan', '4.6', 'The man who inspired billions and billions of physicists (approximately)', 'https://cdn.britannica.com/15/116415-004-27A911EE/Carl-Sagan.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('gmartin', 'George RR', 'Martin', '4.7', 'Likes traumatizing adults', 'https://media.vanityfair.com/photos/53235792932cac31720001be/master/w_2560%2Cc_limit/george-rr-martin.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('jtolkien', 'JRR', 'Tolkien', '4.9', 'Obsessed with jewellery and elves', 'https://cdn.britannica.com/65/66765-050-63A945A7/JRR-Tolkien.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('achristie', 'Agatha', 'Christie', '5.0', 'Undisputed queen of the whoddunnit', 'https://www.biography.com/.image/t_share/MTE5NDg0MDU0OTI0MDY4MzY3/agatha-christie-9247405-1-402.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('mshelley', 'Mary', 'Shelley', '5.0', 'Started a genre in a thunderstorm', 'https://collectionimages.npg.org.uk/large/mw05761/Mary-Shelley.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('dadams', 'Douglas', 'Adams', '4.6', 'A hoopy frood who knows where his towel is', 'https://m.media-amazon.com/images/M/MV5BZTRkZGY3NDAtY2E0Mi00YTJhLWJlYTItZDRlM2JkMGE0ZDc0XkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('epoe', 'Edgar Allan', 'Poe', '4.9', 'The gothiest goth to ever goth', 'https://media.poetryfoundation.org/uploads/media/default/0001/21/bd5c888c4689e6cd3583bbe7575a1a2cad3487f6.jpeg');
+insert into authors (userid, firstname, lastname, avgrating, bio, picture) values ('egaskell', 'Elizabeth', 'Gaskell', '4.6', 'Austens goth twin', 'https://media.newyorker.com/photos/5b84444cc6de535e18043960/1:1/w_1562,h_1562,c_limit/Rosefield-Elizabeth-Gaskell.jpg');
 
--- Population some books
+-- Populating some books
 insert into books (authorid, title, cover, blurb, page_count, genreid) values
 	((select id from authors where lastname like 'Austen'),
 	'Pride and Prejudice',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/91HqOO4DmRL.jpg',
 	'Will-they-wont-they in petticoats',
 	'350',
 	(select id from genres where name like 'romance'));
 insert into books (authorid, title, cover, blurb, page_count, genreid) values
 	((select id from authors where lastname like 'Austen'),
 	'Northanger Abbey',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/817RHZL+9UL.jpg',
 	'How reading too many novels can send delicate young women mildly insane',
 	'190',
 	(select id from genres where name like 'romance'));
 insert into books (authorid, title, cover, blurb, page_count, genreid) values
 	((select id from authors where lastname like 'Gaskell'),
 	'North and South',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/41JJbGrrsnL._SX323_BO1,204,203,200_.jpg',
 	'Look how awful and exploitative 19th century Northern English cotton mills were! But the main characters got married so its a happy ending',
 	'460',
 	(select id from genres where name like 'romance'));
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Shakespeare'),
 	'The Tempest',
-	'url',
+	'https://productimages.worldofbooks.com/185326203X.jpg'
 	'Shipwrecks, monsters, and the meaning of humanity',
 	'500',
 	true,
@@ -125,7 +125,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Shakespeare'),
 	'Hamlet',
-	'url',
+	'https://www.penguin.co.uk/content/dam/prh/books/417/41764/9780141396507.jpg',
 	'The Lion King but with fewer lions',
 	'500',
 	true,
@@ -133,7 +133,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'King'),
 	'It',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/71lZgzNE2kL.jpg',
 	'The OG murderclown, and people fleeing their hometowns',
 	'750',
 	true,
@@ -141,7 +141,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'King'),
 	'Carrie',
-	'url',
+	'https://horrornovelreviews.files.wordpress.com/2013/01/03-06carrie.jpg',
 	'Being a teenage girl is brutal, but the telekinesis has its use',
 	'100',
 	true,
@@ -149,7 +149,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Tolstoy'),
 	'War and Peace',
-	'url',
+	'https://images.penguinrandomhouse.com/cover/9781400079988',
 	'Everyone is going to die',
 	'3000',
 	true,
@@ -157,7 +157,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Dahl'),
 	'Charlie and the Chocolate Factory',
-	'url',
+	'http://prodimage.images-bn.com/pimages/9780142410318_p0_v4_s1200x630.jpg',
 	'All childhood flaws are punishable by death',
 	'78',
 	true,
@@ -165,7 +165,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Dahl'),
 	'James and the Giant Peach',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/81EIIn8uMuL.jpg',
 	'The benefits of alternative, off-grid, plant-based housing',
 	'120',
 	true,
@@ -173,7 +173,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Fleming'),
 	'Casino Royale',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/91JYY6nSK8L.jpg',
 	'The book so good they filmed it twice',
 	'460',
 	true,
@@ -181,15 +181,15 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Fleming'),
 	'From Russia With Love',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/41-FFJynJqL._SX322_BO1,204,203,200_.jpg',
 	'Yesh, Mish Moneypenny, I am 007',
 	'500',
 	true,
 	(select id from genres where name like 'action'));
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Verne'),
-	'20,000 Leagues Under the Sea',
-	'url',
+	'20000 Leagues Under the Sea',
+	'https://images-na.ssl-images-amazon.com/images/I/71uzDFhbgQL.jpg',
 	'Predicting the existence of submarines, and pointing out a lot of fish',
 	'550',
 	true,
@@ -197,7 +197,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Verne'),
 	'Journey to the Center of the Earth',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/911A8d8aCYL.jpg',
 	'Huh, its dark down here',
 	'390',
 	true,
@@ -205,7 +205,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Sagan'),
 	'Cosmos',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/91w4Ci-KMqL.jpg',
 	'The universe is pretty neat',
 	'510',
 	true,
@@ -213,7 +213,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Martin'),
 	'A Game of Thrones',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/91dSMhdIzTL.jpg',
 	'The captivating start to an epic fantasy which will be concluded any day now',
 	'1500',
 	true,
@@ -221,7 +221,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Martin'),
 	'The Winds of Winter',
-	'url',
+	'https://www.parnell.net.nz/wp-content/uploads/2016/11/parnell_watchthisspace_teaser.jpg',
 	'The satisfying final chapter of A Song of Ice and Fire',
 	'10000',
 	true,
@@ -229,7 +229,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Tolkien'),
 	'The Fellowship of the Ring',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/91jBdaRVqML.jpg',
 	'The grandfather of modern high fantasy',
 	'750',
 	true,
@@ -237,7 +237,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Tolkien'),
 	'The Two Towers',
-	'url',
+	'https://m.media-amazon.com/images/I/4123zOAwAgL.jpg',
 	'Starts partway through a long journey. Ends... slightly further along in the same journey',
 	'800',
 	true,
@@ -245,7 +245,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Tolkien'),
 	'The Return of the King',
-	'url',
+	'https://m.media-amazon.com/images/I/41KGl2FqeAL.jpg',
 	'See this, George RR Martin? THIS is how you finish an epic',
 	'1200',
 	true,
@@ -253,7 +253,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Christie'),
 	'The Murder of Roger Ackroyd',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/713gSnLgjbL.jpg',
 	'Spoilers: Monsieur Poirot figures it out',
 	'280',
 	true,
@@ -261,7 +261,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Christie'),
 	'A Murder is Announced',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/619YMl7zdhL.jpg',
 	'Spoilers: Miss Marple figures it out',
 	'500',
 	true,
@@ -269,7 +269,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Shelley'),
 	'Frankenstein',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/51r0PdZrI8L._SX316_BO1,204,203,200_.jpg',
 	'Who is the true monster here?',
 	'510',
 	true,
@@ -277,7 +277,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Adams'),
 	'A Hitchhikers Guide to the Galaxy',
-	'url',
+	'https://images-na.ssl-images-amazon.com/images/I/81XSN3hA5gL.jpg',
 	'Where spaceships hang in the air in the exact way that bricks do not',
 	'300',
 	true,
@@ -285,7 +285,7 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into books (authorid, title, cover, blurb, page_count, approved, genreid) values
 	((select id from authors where lastname like 'Poe'),
 	'The Fall of the House of Usher',
-	'url',
+	'https://images3.penguinrandomhouse.com/cover/9780451530318',
 	'Take care of your family estate, lest it fall into disrepair',
 	'50',
 	true,
@@ -294,3 +294,14 @@ insert into books (authorid, title, cover, blurb, page_count, approved, genreid)
 insert into toread (username, bookid) values ('jausten', 3);
 insert into toread (username, bookid) values ('jausten', 4);
 insert into toread (username, bookid) values ('wshakespeare', 3);
+insert into toread (username, bookid) values ('wshakespeare', 10);
+insert into toread (username, bookid) values ('mshelley', 7);
+insert into toread (username, bookid) values ('mshelley', 25);
+
+insert into haveread (username, bookid) values ('gmartin', 18);
+insert into haveread (username, bookid) values ('gmartin', 19);
+insert into haveread (username, bookid) values ('gmartin', 20);
+insert into haveread (username, bookid) values ('gmartin', 4);
+insert into haveread (username, bookid) values ('gmartin', 5);
+insert into haveread (username, bookid) values ('mshelley', 24);
+insert into haveread (username, bookid) values ('mshelley', 23);
