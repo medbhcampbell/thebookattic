b47c408943d878c4829129936f6a544383dd6368
Medbh Campbell <medbh.campbell53@gmail.com>
2021-02-04 15:51:25 -0600

    Well that took longer than expected

--- thebookattic/author/authordetail.component.tsx
+++ thebookattic/author/authordetail.component.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { View, Text, Image, Pressable, ScrollView } from 'react-native';
-import { Card } from 'react-native-elements';
-import { useSelector} from 'react-redux';
+import { View, Text, Image, ScrollView } from 'react-native';
+import { Rating } from 'react-native-elements';
+import { useSelector } from 'react-redux';
 import { useNavigation } from '@react-navigation/native';
 
 import style from '../global-styles';
 import { AuthorState, BookState } from '../store/store';
+import BookListComponent from '../book/booklist.component';
 
 export default function AuthorDetailComponent() {
     const navigation = useNavigation();
@@ -13,38 +14,20 @@ export default function AuthorDetailComponent() {
     const author = useSelector(selectAuthor);
     const selectBooks = (state: BookState) => state.books;
     const books = useSelector(selectBooks);
-  
-    function onBookSelect(index: number) {
-        const book = books[index];
-        navigation.navigate('BookDetail', book);
-    }
-    
+
     return (
         <ScrollView>
-            <View style={{alignItems: 'center'}}>
-                <Image style={style.authorPreviewImg} source={{uri: author.picture}}/>
-                <Text>{author.firstname + ' ' + author.lastname}</Text>
-                <Text>Average book rating : {author.avgrating}</Text>
+            <View style={style.authorDetailContainer}>
+                <Image style={style.authorPreviewImg} source={{ uri: author.picture }} />
+                <Text style={style.h1}>{author.firstname + ' ' + author.lastname}</Text>
+                <Text>Average book rating :
+                    <Rating ratingBackgroundColor='#F9F9F9' imageSize={20} readonly startingValue={author.avgrating} /></Text>
                 <Text>About : </Text>
                 <Text>{author.bio}</Text>
-                <Text>Books by {author.firstname + ' ' + author.lastname}</Text>
-                {books.map((value, index: number) => {
-                    if (value.authorid === author.id) {
-                        return (
-                            <View>
-                                <Pressable onPress={()=> onBookSelect(index)}>
-                                    <Card>
-                                        <Text style={style.bookPreviewText}>{books[index].title}</Text>
-                                        <Image style={style.bookPreviewImg} source={{uri: books[index].cover}}/>
-                                    </Card>
-                                </Pressable>
-                            </View>
-                        );
-                    } else {
-                        return null;
-                    }
-                })}
             </View>
+            <BookListComponent
+                books={books.filter(item=>item.approved && item.authorid === author.id)}
+                retrievedBooks={true}/>
         </ScrollView>
     )
 }
