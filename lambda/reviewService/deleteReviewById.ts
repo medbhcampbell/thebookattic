e5a502d1c9ae0ea82267e9fd0fb88f51fc95aa94
Lilith Moore <jessejm123@gmail.com>
2021-02-04 17:31:54 -0500

    Average ratings now update on review aprroveal and deletion

--- lambda/reviewService/deleteReviewById.ts
+++ lambda/reviewService/deleteReviewById.ts
@@ -13,8 +13,41 @@ export const handler = async (event: MyEvent): Promise<any> => {
     const client = new Client();
     await client.connect();
     let reviewid= Number(event.path.substring(event.path.lastIndexOf('/')+1, event.path.length));
-    const q = 'delete from reviews where id=$1::integer';
+    // Get the id of the book this review was for
+    let q = `select bookid from reviews where id=$1::int`
+    let bookQuery = await client.query(q, [reviewid]).catch((err: any) => console.error(err));
+    let bookid = bookQuery.rows[0].bookid;
+    
+    // Delete the review
+    q = 'delete from reviews where id=$1::integer';
     let res = await client.query(q, [reviewid]);
+
+    // Check if that was the last review
+    q = 'select * from reviews where bookid=$1::integer';
+    let reviewsLeftQuery = await client.query(q, [bookid]);
+
+    if(reviewsLeftQuery.rows.length > 0) {
+        // Calculate and update author rating
+        q = `update authors set avgrating = 
+                (select avg(r.rating) from reviews r inner join books b on (r.bookid = b.id) where b.authorid=
+                    (select authorid from books where id=$1::int))
+            where id = 
+                (select authorid from books where id=$2::int);`;
+        await client.query(q, [bookid,bookid]).catch((err: any) => console.error(err));
+
+        // Calculate and update book rating
+        q = `update books set rating = (select avg(rating) from reviews where bookid=$1::int) where id=$2::int;`;
+        await client.query(q, [bookid,bookid]).catch((err: any) => console.error(err));
+    }
+    else {
+        // If this was the last review, set the average ratings to 0. Otherwise avg() would set them to null
+        q = `update authors set avgrating=0 where id=(select authorid from books where id=$1::int);`;
+        await client.query(q, [bookid]).catch((err: any) => console.error(err));
+
+        q = `update books set rating=0 where id=$1::int;`;
+        await client.query(q, [bookid]).catch((err: any) => console.error(err));
+    }
+
     await client.end();
     const head = {
         'Content-Type': 'application/json',
