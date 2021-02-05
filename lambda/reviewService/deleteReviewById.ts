1932fa20d1d78da0da88dfedcfd3d82917417ea5
Medbh Campbell <medbh.campbell53@gmail.com>
2021-02-05 16:01:05 -0600

    oops, should have changed integer to int, not the other way around

--- lambda/reviewService/deleteReviewById.ts
+++ lambda/reviewService/deleteReviewById.ts
@@ -14,37 +14,37 @@ export const handler = async (event: MyEvent): Promise<any> => {
     await client.connect();
     let reviewid= Number(event.path.substring(event.path.lastIndexOf('/')+1, event.path.length));
     // Get the id of the book this review was for
-    let q = `select bookid from reviews where id=$1::integer`
+    let q = `select bookid from reviews where id=$1::int`
     let bookQuery = await client.query(q, [reviewid]).catch((err: any) => console.error(err));
     let bookid = bookQuery.rows[0].bookid;
     
     // Delete the review
-    q = 'delete from reviews where id=$1::integer';
+    q = 'delete from reviews where id=$1::int';
     let res = await client.query(q, [reviewid]);
 
     // Check if that was the last review
-    q = 'select * from reviews where bookid=$1::integer';
+    q = 'select * from reviews where bookid=$1::int';
     let reviewsLeftQuery = await client.query(q, [bookid]);
 
     if(reviewsLeftQuery.rows.length > 0) {
         // Calculate and update author rating
         q = `update authors set avgrating = 
                 (select avg(r.rating) from reviews r inner join books b on (r.bookid = b.id) where b.authorid=
-                    (select authorid from books where id=$1::integer))
+                    (select authorid from books where id=$1::int))
             where id = 
-                (select authorid from books where id=$2::integer);`;
+                (select authorid from books where id=$2::int);`;
         await client.query(q, [bookid,bookid]).catch((err: any) => console.error(err));
 
         // Calculate and update book rating
-        q = `update books set rating = (select avg(rating) from reviews where bookid=$1::integer) where id=$2::integer;`;
+        q = `update books set rating = (select avg(rating) from reviews where bookid=$1::int) where id=$2::int;`;
         await client.query(q, [bookid,bookid]).catch((err: any) => console.error(err));
     }
     else {
         // If this was the last review, set the average ratings to 0. Otherwise avg() would set them to null
-        q = `update authors set avgrating=0 where id=(select authorid from books where id=$1::integer);`;
+        q = `update authors set avgrating=0 where id=(select authorid from books where id=$1::int);`;
         await client.query(q, [bookid]).catch((err: any) => console.error(err));
 
-        q = `update books set rating=0 where id=$1::integer;`;
+        q = `update books set rating=0 where id=$1::int;`;
         await client.query(q, [bookid]).catch((err: any) => console.error(err));
     }
 
